2c4fa4a3aad01474cf115c09e1c61b1082bf9b7d
Freanu Peria <magnussonfrancis@gmail.com>
2019-09-14 14:56:02 +0800

    added login page and form.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+enum FormMode { LOGIN, SIGNUP }
+
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.indigo
+      ),
       title: 'Ink Me Up!',
       home: Splash(),
 
@@ -16,7 +21,7 @@ class MyApp extends StatelessWidget{
 class Splash extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    if (1==1){ // in this condition, it should check if user is authenticated.
+    if (1==2){ // in this condition, it should check if user is authenticated.
       print('');
       return Scaffold(
         appBar: AppBar(
@@ -25,13 +30,185 @@ class Splash extends StatelessWidget{
         body: Text('Welcome/home page here'),
       );
     }else{
-      print('not yet authenticated');
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Ink Me Up!'),
-        ),
-        body: Text('login/signup here'),
-      );
+      return LoginSignUpPage();
     }
   }
 }
+
+class LoginSignUpPage extends StatefulWidget{
+  @override
+  State createState() => _LoginSignUpPageState();
+}
+
+class _LoginSignUpPageState extends State<LoginSignUpPage>{
+
+  var _email = '';
+  var _password = '';
+  var _isLoading = false;
+  var _formMode = FormMode.LOGIN;
+  var _errorMessage = '';
+  final _formKey = GlobalKey<FormState>();
+
+  _validateAndSubmit(){
+    print('validating');
+  }
+
+
+  Widget _showCircularProgress(){
+    if (_isLoading){
+      return Center(child: CircularProgressIndicator(),);
+    }
+    return Container(height: 0.0, width: 0.0,);
+  }
+  
+  Widget _showLogo(){
+    return Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 80.0,
+          child: Image.asset('assets/icon/inkmeup.png'),
+        ),
+      ),
+    );
+  }
+
+  Widget _showEmailInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+      child: TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: InputDecoration(
+            hintText: 'Email',
+            icon: Icon(
+              Icons.mail,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _email = value.trim(),
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+      child: TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: InputDecoration(
+            hintText: 'Password',
+            icon: Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value.trim(),
+      ),
+    );
+  }
+
+  Widget _showPrimaryButton() {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
+        child: MaterialButton(
+          elevation: 5.0,
+          minWidth: 200.0,
+          height: 42.0,
+          color: Colors.indigo,
+          child: _formMode == FormMode.LOGIN
+              ? Text('Login',
+              style: TextStyle(fontSize: 20.0, color: Colors.white))
+              : Text('Create account',
+              style: TextStyle(fontSize: 20.0, color: Colors.white)),
+          onPressed: _validateAndSubmit,
+        ));
+  }
+
+  Widget _showSecondaryButton() {
+    return new FlatButton(
+      child: _formMode == FormMode.LOGIN
+          ? new Text('Create an account',
+          style: new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300))
+          : new Text('Have an account? Sign in',
+          style:
+          new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300)),
+      onPressed: _formMode == FormMode.LOGIN
+          ? _changeFormToSignUp
+          : _changeFormToLogin,
+    );
+  }
+
+  void _changeFormToSignUp() {
+    _formKey.currentState.reset();
+    _errorMessage = "";
+    setState(() {
+      _formMode = FormMode.SIGNUP;
+    });
+  }
+
+  void _changeFormToLogin() {
+    _formKey.currentState.reset();
+    _errorMessage = "";
+    setState(() {
+      _formMode = FormMode.LOGIN;
+    });
+  }
+
+  Widget _showErrorMessage() {
+    if (_errorMessage.length > 0 && _errorMessage != null) {
+      return Text(
+        _errorMessage,
+        style: TextStyle(
+            fontSize: 13.0,
+            color: Colors.red,
+            height: 1.0,
+            fontWeight: FontWeight.w300),
+      );
+    } else {
+      return Container(
+        height: 0.0,
+      );
+    }
+  }
+
+  Widget _showBody(){
+    return Container(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              _showLogo(),
+              _showEmailInput(),
+              _showPasswordInput(),
+              _showPrimaryButton(),
+              _showSecondaryButton(),
+              _showErrorMessage(),
+            ],
+          ),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Login/Sign Up'),),
+      ),
+      body: Stack(
+        children: <Widget>[
+          _showBody(),
+          _showCircularProgress(),
+        ],
+      ),
+    );
+  }
+}
+
